c26da98c2c89f77e94cccb19222c97490e098b2a
lauri piikivi <macaque-04-step@icloud.com>
2025-03-31 15:14:31 +0300

    Update main.c
    more raw format, no logging, to forward into wireshark

--- dect-nr-samples/sniffer/src/main.c
+++ dect-nr-samples/sniffer/src/main.c
@@ -2,258 +2,370 @@
 Copyright (c) 2023  Nordic Semiconductor ASA
 SPDX-License-Identifier: BSD-3-Clause
 *******************************************************************************
-Simple sniffer, prints out messages seen 
+Simple sniffer, prints out messages seen on the air DECT-2020
+
+MUST CONFIGURE the newtork ID to descramble messages. default is network_id=0x12345678
+as used in dect-shell sample form nordic.
+Prepends strings with H == PCC header and length byte in HEX
+  PCC error is H00
+Prepends strings with P == PDC header and 2 length bytes in HEX
+  PDC error is P0000
+these values are easy to remove from the hex strings in PC side before sending to
+wireshark or similar.
 ****************************************************************************
 TODO:
-- 
+-
 ****************************************************************************/
 
 #include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <dk_buttons_and_leds.h>
 #include <nrf_modem_dect_phy.h>
 #include <modem/nrf_modem_lib.h>
 
-
 LOG_MODULE_REGISTER(app);
 
-//state flags
-//switch between RX or TX role
-int RECEIVER_ROLE = 1;
-int INIT_DONE = 0;
-int EXIT =0;
-
-//handle values for API calls, separate tx and rx
-int txHandle = 1;
+// handle values for API calls, separate tx and rx
 int rxHandle = 31400;
-// statistics collecting
-int previous_received=-1;
-int missing_errors=0;
-int crc_errors=0;
-int received_ok =0;
-float rssi_average = 0;
-int n=0;
+int EXIT = 0;
+uint8_t pcc_data[21];
+uint8_t pdc_data[1200];
+uint8_t hdr[10];
 
-//Note that the MCS impacts how much data can be fit into subslots/slots
-#define MCS  1
-// tx power, 0x0d == 19 dBm, maximum of the HW
-#define POWER  0x0d
-//this depends on the MCS above
-#define DATA_LEN 137
-//MCS 4, 4 subslots
-//#define DATA_LEN 407
-#define PACKET_LEN 4 //subslots
-uint8_t _txData[DATA_LEN];
-uint8_t _rxData[DATA_LEN];
-//NOTE !!! defining  carrier to use, please modify according to region and scan results
+// Note that the MCS impacts how much data can be fit into subslots/slots
+#define MCS 1
 #define CARRIER 1677
 
-//semaphore for API calls, only 1 async operation at time in this sample 
-K_SEM_DEFINE(modem, 1, 1);
+// semaphore for API calls, only 1 async operation at time in this sample
+K_SEM_DEFINE(operation_sem, 1, 1);
 
-//TYPE 1 (SHORT BROADCAST) FORMAT 000 header for messages used
-struct phy_ctrl_field_common
+static struct nrf_modem_dect_phy_config_params dect_phy_config_params = {
+    .band_group_index = 0,
+    .harq_rx_process_count = 4,
+    .harq_rx_expiry_time_us = 5000000};
+
+/* Callback after init operation. */
+static void on_init(const struct nrf_modem_dect_phy_init_event *evt)
 {
-    uint8_t packet_length           : 4;
-    uint8_t packet_length_type      : 1;
-    uint8_t header_format           : 3;
-    uint8_t short_network_id;
-    uint8_t transmitter_id_hi;
-    uint8_t transmitter_id_lo;
-    uint8_t df_mcs         : 3;
-    uint8_t reserved       : 1;
-    uint8_t transmit_power : 4;
-};
-union nrf_modem_dect_phy_hdr phyHeader;
-
-//new parameters for HARQ operation, not used in this sample
-const struct nrf_modem_dect_phy_init_params init_params ={
-	.harq_rx_expiry_time_us=5000000,
-	.harq_rx_process_count=1
-};
-
-
-//exit on ANY button on the devkit
-void button_handler(uint32_t button_state, uint32_t has_changed)
-{
-    EXIT=1;
-}
-
-
-// Callback functions from PHY API
-void init(const uint64_t *time, int16_t temp, enum nrf_modem_dect_phy_err err, const struct nrf_modem_dect_phy_modem_cfg *cfg)
-{
-  if(err==0) {
-    LOG_INF("DECT Init done, temperature %d", temp);
+  if (evt->err)
+  {
+    printk("ERROR Init failed, err %d", evt->err);
+    EXIT = true;
+    return;
   }
-  else {
-    LOG_ERR("INIT FAILED");
-    printk("Init failed, Exit\n");
-    EXIT=1;
+
+  k_sem_give(&operation_sem);
+}
+
+/* Callback after deinit operation. */
+static void on_deinit(const struct nrf_modem_dect_phy_deinit_event *evt)
+{
+  if (evt->err)
+  {
+    printk("ERROR Deinit failed, err %d", evt->err);
+    return;
   }
-  k_sem_give(&modem);
+
+  k_sem_give(&operation_sem);
 }
 
-void op_complete(const uint64_t *time, int16_t temperature, enum nrf_modem_dect_phy_err err, uint32_t handle)
+static void on_activate(const struct nrf_modem_dect_phy_activate_event *evt)
 {
-  LOG_DBG("operation_complete_cb Status %d, Temp %d, Handle %d", err, temperature, handle);
-  k_sem_give(&modem);
-  return;
+  if (evt->err)
+  {
+    printk("ERROR Activate failed, err %d", evt->err);
+    EXIT = true;
+    return;
+  }
+
+  k_sem_give(&operation_sem);
 }
 
-void rssi(const uint64_t *time, const struct nrf_modem_dect_phy_rssi_meas *status)
-{   
-  k_sem_give(&modem);
-  return;
-}
-
-
-void rx_stop(const uint64_t *time, enum nrf_modem_dect_phy_err err, uint32_t handle)
+static void on_deactivate(const struct nrf_modem_dect_phy_deactivate_event *evt)
 {
-  LOG_DBG("operation_stop_cb Status %d Handle %d", err, handle);
-  k_sem_give(&modem);
-  return;
+
+  if (evt->err)
+  {
+    printk("ERROR Deactivate failed, err %d", evt->err);
+    return;
+  }
+
+  k_sem_give(&operation_sem);
 }
 
-//phy header receive
-void pcc(
-  const uint64_t *time,
-  const struct nrf_modem_dect_phy_rx_pcc_status *status,
-  const union nrf_modem_dect_phy_hdr *hdr)
+static void on_configure(const struct nrf_modem_dect_phy_configure_event *evt)
 {
-  int16_t resp=status->rssi_2 / 2;
-  LOG_INF("pcc_cb phy_header_valid %d rssi_2 %d", status->header_status, resp);
-  LOG_HEXDUMP_INF(hdr, sizeof(*hdr), "\tRX PCC: ");
-  return;
+  if (evt->err)
+  {
+    printk("ERROR Configure failed callback err %d", evt->err);
+    return;
+  }
+
+  k_sem_give(&operation_sem);
 }
 
-void pcc_crc_err(const uint64_t *time, const struct nrf_modem_dect_phy_rx_pcc_crc_failure *crc_failure)
-{
-  LOG_DBG("PCC CRC ERROR, rssi_2, %d", crc_failure->rssi_2/2);
-}
-
-//data paylod receive, statistics calculation and tracking previous received message number
-void pdc(
-  const uint64_t *time,
-  const struct nrf_modem_dect_phy_rx_pdc_status *status,
-  const void *data, uint32_t len)
-{
- 
-  LOG_INF("PDC DATA, LEN %d, rssi_2 %d, SNR %d", len, status->rssi_2/2, status->snr);
-  LOG_HEXDUMP_INF(data, len, "\tRX PDC: ");
-
-}
-
-void pdc_crc_err(
-  const uint64_t *time, const struct nrf_modem_dect_phy_rx_pdc_crc_failure *pdc_failure)
-{
-  crc_errors++;
-  LOG_DBG("PDC CRC ERROR, rssi_2, %d", pdc_failure->rssi_2/2);
-}
-
-void link_config(const uint64_t *time, enum nrf_modem_dect_phy_err err)
+/* Callback after link configuration operation. */
+static void on_link_config(const struct nrf_modem_dect_phy_link_config_event *evt)
 {
   return;
 }
 
-void time_get(const uint64_t *time, enum nrf_modem_dect_phy_err err)
+static void on_radio_config(const struct nrf_modem_dect_phy_radio_config_event *evt)
 {
-  LOG_DBG("Time query response time %"PRIu64" Status %d", *time, err); 
+  if (evt->err)
+  {
+    printk("ERROR Radio config failed, err %d", evt->err);
+    return;
+  }
+
+  k_sem_give(&operation_sem);
 }
 
-void capability_get(const uint64_t *time, enum nrf_modem_dect_phy_err err,const struct nrf_modem_dect_phy_capability *capability)
+/* Callback after capability get operation. */
+static void on_capability_get(const struct nrf_modem_dect_phy_capability_get_event *evt)
 {
-  LOG_DBG("Capability query response FIXME %"PRIu64" Status %d", *time, err); 
+  return;
 }
 
-void deinit(const uint64_t *time, enum nrf_modem_dect_phy_err err)
+static void on_bands_get(const struct nrf_modem_dect_phy_band_get_event *evt)
 {
-  LOG_DBG("DEINIT response time %"PRIu64" Status %d", *time, err); 
+  return;
 }
 
-//set function callbacks to structure, registered to  modem API  in main
-struct nrf_modem_dect_phy_callbacks dect_cb_config = {
-    .init = init,
-    .op_complete = op_complete,
-    .rssi = rssi,
-    .rx_stop = rx_stop,
-    .pcc = pcc,
-    .pcc_crc_err = pcc_crc_err,
-    .pdc = pdc,
-    .pdc_crc_err = pdc_crc_err,
-    .link_config = link_config,
-    .time_get = time_get,
-    .capability_get = capability_get,
-    .deinit=deinit
-};
+static void on_latency_info_get(const struct nrf_modem_dect_phy_latency_info_event *evt)
+{
+  return;
+}
 
-//listen, start immediately and listen for time_s duration
+/* Callback after time query operation. */
+static void on_time_get(const struct nrf_modem_dect_phy_time_get_event *evt)
+{
+  return;
+}
+
+static void on_cancel(const struct nrf_modem_dect_phy_cancel_event *evt)
+{
+
+  k_sem_give(&operation_sem);
+  return;
+}
+
+/* Operation complete notification. */
+static void on_op_complete(const struct nrf_modem_dect_phy_op_complete_event *evt)
+{
+  k_sem_give(&operation_sem);
+  return;
+}
+
+/* Physical Control Channel reception notification. */
+static void on_pcc(const struct nrf_modem_dect_phy_pcc_event *evt)
+{
+  union nrf_modem_dect_phy_hdr hdr=evt->hdr;
+  int i=0;
+  int l = 0;
+  if (evt->phy_type == 1)
+  {
+    l = 10;
+    for (i = 0; i < l; i++)
+    {
+      sprintf(&pcc_data[i * 2], "%02X", hdr.type_2[i]);
+    }
+  }
+  else
+  {
+    l = 5;
+    for (i = 0; i < l; i++)
+    {
+      sprintf(&pcc_data[i * 2], "%02X", hdr.type_1[i]);
+    }
+  }
+  printk("H%02X%s\n", l, pcc_data);
+  return;
+}
+
+/* Physical Control Channel CRC error notification. */
+static void on_pcc_crc_err(const struct nrf_modem_dect_phy_pcc_crc_failure_event *evt)
+{
+  printk("H%02X\n", 0);
+}
+
+/* Physical Data Channel reception notification. */
+static void on_pdc(const struct nrf_modem_dect_phy_pdc_event *evt)
+{
+  int i;
+  for (i = 0; i < evt->len; i++)
+  {
+    sprintf(&pdc_data[i * 2], "%02X", ((uint8_t *)evt->data)[i]);
+  }
+  printk("P%s\n", pdc_data);
+  return;
+}
+
+/* Physical Data Channel CRC error notification. */
+static void on_pdc_crc_err(const struct nrf_modem_dect_phy_pdc_crc_failure_event *evt)
+{
+  printk("P%04X%s\n", 0);
+}
+
+/* RSSI measurement result notification. */
+static void on_rssi(const struct nrf_modem_dect_phy_rssi_event *evt)
+{
+  return;
+}
+
+static void on_stf_cover_seq_control(const struct nrf_modem_dect_phy_stf_control_event *evt)
+{
+  return;
+}
+
+static void dect_phy_event_handler(const struct nrf_modem_dect_phy_event *evt)
+{
+
+  switch (evt->id)
+  {
+  case NRF_MODEM_DECT_PHY_EVT_PCC:
+    on_pcc(&evt->pcc);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_PDC:
+    on_pdc(&evt->pdc);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_PCC_ERROR:
+    on_pcc_crc_err(&evt->pcc_crc_err);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_PDC_ERROR:
+    on_pdc_crc_err(&evt->pdc_crc_err);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_INIT:
+    on_init(&evt->init);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_DEINIT:
+    on_deinit(&evt->deinit);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_ACTIVATE:
+    on_activate(&evt->activate);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_DEACTIVATE:
+    on_deactivate(&evt->deactivate);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_CONFIGURE:
+    on_configure(&evt->configure);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_RADIO_CONFIG:
+    on_radio_config(&evt->radio_config);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_COMPLETED:
+    on_op_complete(&evt->op_complete);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_CANCELED:
+    on_cancel(&evt->cancel);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_RSSI:
+    on_rssi(&evt->rssi);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_TIME:
+    on_time_get(&evt->time_get);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_CAPABILITY:
+    on_capability_get(&evt->capability_get);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_BANDS:
+    on_bands_get(&evt->band_get);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_LATENCY:
+    on_latency_info_get(&evt->latency_get);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_LINK_CONFIG:
+    on_link_config(&evt->link_config);
+    break;
+  case NRF_MODEM_DECT_PHY_EVT_STF_CONFIG:
+    on_stf_cover_seq_control(&evt->stf_cover_seq_control);
+    break;
+  }
+}
+
+// listen, start immediately and listen for time_s duration
 void modem_rx(uint32_t rxMode, int time_s)
 {
   // Setup the nrf_modem_dect_phy_operation_rx
-  struct nrf_modem_dect_phy_rx_params rxOpsParams={0};
-  rxOpsParams.start_time = 0; //start immediately
+  struct nrf_modem_dect_phy_rx_params rxOpsParams = {0};
+  rxOpsParams.start_time = 0; // start immediately
   rxOpsParams.handle = rxHandle;
-  rxOpsParams.network_id=0;
+  rxOpsParams.network_id = 0x12345678;
   rxOpsParams.mode = rxMode;
+  rxOpsParams.rssi_interval = NRF_MODEM_DECT_PHY_RSSI_INTERVAL_OFF;
   rxOpsParams.link_id = NRF_MODEM_DECT_PHY_LINK_UNSPECIFIED;
-  rxOpsParams.rssi_level = -60;
+  rxOpsParams.rssi_level = 0;
   rxOpsParams.carrier = CARRIER;
   // modem clock ticks NRF_MODEM_DECT_MODEM_TIME_TICK_RATE_KHZ --> 69120*1000* TIME_S
-  rxOpsParams.duration = time_s*69120*1000; 
-  //filter on the short network id, last 8 bits of the network identifier in dect nr
-  rxOpsParams.filter.short_network_id = (uint8_t)(0x0a);
+  rxOpsParams.duration = time_s * 69120 * 1000;
+  // filter on the short network id, last 8 bits of the network identifier in dect nr
+  rxOpsParams.filter.short_network_id = (uint8_t)(0xaa);
   rxOpsParams.filter.is_short_network_id_used = 0;
-  //listen for everything (broadcast mode usedd)
+  // listen for everything (broadcast mode usedd)
   rxOpsParams.filter.receiver_identity = 0;
-  k_sem_take(&modem, K_FOREVER);
-  int err=nrf_modem_dect_phy_rx(&rxOpsParams);
-  if(err!=0) LOG_ERR("RX FAIL %d", err);
-  if(rxHandle==65000)rxHandle=31400;
-  else rxHandle++;
+  k_sem_take(&operation_sem, K_FOREVER);
+  int err = nrf_modem_dect_phy_rx(&rxOpsParams);
+  if (err != 0)
+    printk("RX FAIL %d", err);
+  if (rxHandle == 65000)
+    rxHandle = 31400;
+  else
+    rxHandle++;
 }
 
-
 int main(void)
-  {
+{
+  int err = 0;
+
   k_msleep(100);
-  //send an increasing counter, start from 1 
 
-  //using printk, will show something even if logging filtering is not set correctly
-  printk("START DECT SAMPLE\n");
-  dk_buttons_init(button_handler);
-  printk("Buttons init\t");
-  dk_leds_init();
-  printk("leds init\t");
-  
-  nrf_modem_lib_init();
-  
-  k_sem_take(&modem, K_FOREVER);
-  int err=0;
-  err=nrf_modem_dect_phy_callback_set(&dect_cb_config);
-  if(err!=0) {
-    printk("ERROR settings callbacks %d\n",err);
-  }
-  err=nrf_modem_dect_phy_init(&init_params);
-  if(err!=0) {
-    printk("ERROR initializing modem PHY %d\n",err);
-    return -1;
-  }
-  printk("DECT init started\n");
-  
-  printk("Listening on channel %d for transmissions\n", CARRIER);
-  
-  // RX ROLE, LOOP
-  dk_set_led_on(DK_LED1);
-  dk_set_led_on(DK_LED2);
-  printk("RX ROLE\n");
 
-  while(EXIT==0){
-    //loop RX mode   
-    modem_rx(NRF_MODEM_DECT_PHY_RX_MODE_CONTINUOUS, 2);
+  err = nrf_modem_lib_init();
+  if (err)
+  {
+    printk("modem init failed, err %d\n", err);
+    return err;
   }
-  //messages may be in logging pipeline, wait a sec
+  
+  err = nrf_modem_dect_phy_event_handler_set(dect_phy_event_handler);
+  if (err)
+  {
+    printk("nrf_modem_dect_phy_event_handler_set failed, err %d\n", err);
+    return err;
+  }
+
+  k_sem_take(&operation_sem, K_FOREVER);
+  err = nrf_modem_dect_phy_init();
+  if (err)
+  {
+    printk("nrf_modem_dect_phy_init failed, err %d\n", err);
+    return err;
+  }
+
+  k_sem_take(&operation_sem, K_FOREVER);
+  err = nrf_modem_dect_phy_configure(&dect_phy_config_params);
+  if (err)
+  {
+    printk("nrf_modem_dect_phy_configure failed, err %d\n", err);
+    return err;
+  }
+
+  k_sem_take(&operation_sem, K_FOREVER);
+  err = nrf_modem_dect_phy_activate(NRF_MODEM_DECT_PHY_RADIO_MODE_LOW_LATENCY);
+  if (err)
+  {
+    printk("nrf_modem_dect_phy_activate failed, err %d\n", err);
+    return err;
+  }
+
+  printk("DECT sniffer listening on channel %d\n", CARRIER);
+
+  while (0 == EXIT)
+  {
+    // loop RX mode
+    modem_rx(NRF_MODEM_DECT_PHY_RX_MODE_CONTINUOUS, 10);
+  }
+  // messages may be in logging pipeline, wait a sec
   k_msleep(1000);
   printk("\nEXIT Listening\n");
   return 0;
