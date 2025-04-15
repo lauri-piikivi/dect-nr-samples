1f7360dc17ab8c955ee4ef833188c0e9550d86cc
lauri piikivi <macaque-04-step@icloud.com>
2025-04-15 14:21:52 +0300

    Update desniff.py
    v0.9 writes the config for sniffer on devkit

--- dect-nr-samples/sniffer/python/desniff.py
+++ dect-nr-samples/sniffer/python/desniff.py
@@ -9,12 +9,22 @@ import ipaddress
 """
 Listens on serial line for DECT messages and forwards them to a multicast group for wireshark capturing
 Format for serial line string is:
-H + 1 byte length  in hex + PPC header hex
-P + PDC payload hex bytes
-The PCC hdr length  is needed as PPC header type 1 and type 2 both have format 000 as exact same header value
+  W
+  write configuration , NETWORK_ID CARRIER
+  
+  H for PCC Header
+  Type_1 (dect nr+ beacon header) 5 bytes + filled with  0x00 to make 10 byte header
+  Type_2 (dect nr+ unicast) 10 byte header
+  PCC error is H0000
 
-Forwards to (multicast)IP, to be shown, saved in Wireshark a UDP packet
-UDP Packet payload [PCC header length, PCC, PDC] as raw bytes
+Prepends strings with P == PDC header 
+  P for PDC payload, variable length
+  PDC error is P0000
+  op_complete in device prints plain P for cases where only header (ACK) is received (MCS 0, len == 0)
+
+Script strips the H and P identifiers 
+Forwards to (multicast)IP, to be shown in Wireshark a UDP packet, can be saved in wireshark
+UDP Packet payload [PCC 10B header, PDC] as raw bytes
 UDP packet can be decoded in wireshark by port number
 ===========================================================================
 FUTURE: give network ID from serial to device
@@ -25,11 +35,13 @@ start_time = 0
 ser = None
 multicast_ip = None
 udp_port = None
+network_id = None
+carrier = None
 sock = None
 sthread = None
 stop_event = threading.Event()
-debug=True
-hdr_line=None
+debug= None
+hdr_line=""
 
 def createSocket():
     # Create UDP socket
@@ -51,19 +63,31 @@ def send_multicast(data: bytes):
         print(f"An error occurred while sending multicast: {str(e)}")
         exit()
 
+# only used at start to write configuration to device
+def write_serial():
+    ser.write(network_id.to_bytes(4, byteorder='little'))
+    ser.write(carrier.to_bytes(2, byteorder='little'))
+    return
+
 def parse(line: str):
     global hdr_line
     line=line.strip()
+    if (debug and len(line)>1): print(line)
+
+    #waits for configuration
+    if(line.startswith("W")):
+        write_serial()
+
     if line.startswith("H"):
         line=line.strip('H')
         hdr_line = line
-        #wait for PDC for the header
+        #wait for PDC
 
     if line.startswith("P"):
         line=line.strip('P')
         packet=hdr_line+line
-        send_multicast(bytes.fromhex(packet))
-        if(debug):print(packet)
+        if(len(packet)>0):send_multicast(bytes.fromhex(packet))
+        hdr_line=""
     return
     
 def read_serial():
@@ -97,6 +121,8 @@ def main():
     global start_time
     global sthread
 
+    createSocket()
+
     print(f"Starting DECT sniffer on serial port {serial_port}")
     try:
         start_time = str(int(time.time()))
@@ -119,12 +145,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read data from a serial port and send in UDP for wireshark capturing')
     parser.add_argument('-s','--serial_port', type=str, default="/dev/tty.usbmodem0010512017591",help='Serial port identifier (e.g., COM1, /dev/ttyUSB0)')
     parser.add_argument('-m', '--multicast_ip', type=ipaddress.IPv4Address, default="224.0.31.41", help='multicast IPv4 to send captured DECT messages to')
-    parser.add_argument('-u','--udp_port', type=int, default=31414, help='UDP port to send ßto')
-  
+    parser.add_argument('-u','--udp_port', type=int, default=31414, help='UDP port to send to')
+    parser.add_argument('-n', '--network_id', type=int, default=0x12345678, help='NETWORK_ID HEX')
+    parser.add_argument('-c', '--carrier', type=int, default=1663, help='CARRIER to listen on')
+    parser.add_argument('-d', '--debug', type=bool, default=True, help='Print messages on terminal')
+
     args = parser.parse_args()
 
     serial_port = args.serial_port
     multicast_ip=args.multicast_ip
     udp_port=args.udp_port
-
+    network_id=args.network_id
+    carrier=args.carrier
+    debug=args.debug
     main()
